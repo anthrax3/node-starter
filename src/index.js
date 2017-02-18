610b4601a6d8c713f60e2846b45740ace9c2926d
helfi92 <helfi92@gmail.com>
2017-02-18 11:40:41 -0500

    Dedent 2 spaces

--- src/index.js
+++ src/index.js
@@ -5,4 +5,4 @@ const port = process.env.PORT || 3000;
 createServer((request, response) => {
   response.write('<div style="padding: 20px"><h1>Neutrino</h1><p>Welcome to Node</p></div>');
 })
-  .listen(port, () => console.log(`Running on :${port}`));
+.listen(port, () => console.log(`Running on :${port}`));
